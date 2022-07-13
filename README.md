c01cac7b77033740137736848be73d68245645ca
Najibulloh Shamsudinov <najibullohshamsudinzoda@gmail.com>
2022-07-13 08:45:14 +0500

    Task 1 - Area

---
+++ README.md
@@ -0,0 +1,3 @@
+# Task 1 - Area
+
+Calculate of area.
---
+++ main.js
@@ -0,0 +1,6 @@
+let a = +prompt();
+let b = +prompt();
+const pi = 3.14;
+alert("S =" + a * b + "  P = " + a / b);
+alert(`S = ${a * b}  P =  ${a / b}  N= ${a * p}`);
+    
